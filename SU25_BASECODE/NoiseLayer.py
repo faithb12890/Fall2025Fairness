73b5cd31a0b1ef1f67db3b1590a166586add8629
faithb12890 <faithbergstrom@gmail.com>
2025-11-29 16:08:27 -0800

    Normalization and noising layer created using mu and sigma vectors from ave_stdev_finder

---
+++ SU25_BASECODE/NoiseLayer.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+# The Adult data from the installed module is formatted in the following way:
+# Col 0: age (normalized to normal gaussian distribution with mean of 0, standard dev of 1)
+# Col 1-7: workclass
+#   [Private, Self-emp-not-inc, Self-emp-inc, Federal-gov, Local-gov, State-gov, Without-pay]
+# Col 8: fnlwgt (normalized)
+# Col 9-24: education
+#   [Bachelors, Some-college, 11th, HS-grad, Prof-school, Assoc-acdm, Assoc-voc, 9th, 7th-8th, 12th, Masters, 1st-4th, 10th, Doctorate, 5th-6th, Preschool]
+# Col 25: education-num (normalized)
+# Col 26-32: marital-status
+#   [Married-civ-spouse, Divorced, Never-married, Separated, Widowed, Married-spouse-absent, Married-AF-spouse]
+# Col 33-46: occupation
+#   [Tech-support, Craft-repair, Other-service, Sales, Exec-managerial, Prof-specialty, Handlers-cleaners, Machine-op-inspct, Adm-clerical, Farming-fishing, Transport-moving, Priv-house-serv, Protective-serv, Armed-Forces]
+# Col 47-52: relationship
+#   [Wife, Own-child, Husband, Not-in-family, Other-relative, Unmarried, ]
+# Col 53-57: race
+#   [White, Asian-Pac-Islander, Amer-Indian-Eskimo, Other, Black]
+# Col 58-59: sex 
+#   [Female, Male]
+# Col 60: capital-gain (normalized)
+# Col 61: capital-loss (normalized)
+# Col 62: hours-per-week (normalized)
+# Col 63-103: native-country
+#   [United-States, Cambodia, England, Puerto-Rico, Canada, Germany, Outlying-US(Guam-USVI-etc), India, Japan, Greece, South, China, Cuba, Iran, Honduras, Philippines, Italy, Poland, Jamaica, Vietnam, Mexico, Portugal, Ireland, France, Dominican-Republic, Laos, Ecuador, Taiwan, Haiti, Columbia, Hungary, Guatemala, Nicaragua, Scotland, Thailand, Yugoslavia, El-Salvador, Trinadad&Tobago, Peru, Hong, Holand-Netherlands]
+# Col 104: income (target)
+
+# try to read normalization mu and sigma vectors from row in csv
+
+class NormalNoise(nn.Module):
+    '''
+    Custom neural net layer to normalize input data and add noise if smooth flag
+    is set to true.
+    '''
+    def __init__(self, sigma=0.2, smooth=False):
+        self.sigma = sigma      # Sigma value for noise. 0.2 by default.
+        self.smooth = smooth    # Whether or not to smooth inputs. False by default.
+        self.smoothvars = torch.zeros(103)
+        # Selecting variables to smooth over
+        self.smoothvars[0] = 1      # age
+        self.smoothvars[26:33] = 1  # marital-status
+        self.smoothvars[53:58] = 1  # race
+        self.smoothvars[58:60] = 1  # sex
+        self.normalmu = torch.tensor([-1.7706e-09,  7.3888e-01,  8.2853e-02,  3.5608e-02,  3.1265e-02,
+         6.8530e-02,  4.2404e-02,  4.6416e-04, -9.4855e-10,  1.6723e-01,
+         2.2140e-01,  3.4746e-02,  3.2624e-01,  1.7970e-02,  3.3420e-02,
+         4.3333e-02,  1.5085e-02,  1.8467e-02,  1.2499e-02,  5.3942e-02,
+         5.0063e-03,  2.7187e-02,  1.2433e-02,  9.5484e-03,  1.4919e-03,
+         5.0589e-09,  4.6632e-01,  1.3971e-01,  3.2246e-01,  3.1132e-02,
+         2.7419e-02,  1.2267e-02,  6.9624e-04,  3.0237e-02,  1.3361e-01,
+         1.0649e-01,  1.1883e-01,  1.3235e-01,  1.3388e-01,  4.4758e-02,
+         6.5181e-02,  1.2337e-01,  3.2790e-02,  5.2119e-02,  4.7411e-03,
+         2.1351e-02,  2.9839e-04,  4.6615e-02,  1.4807e-01,  4.1320e-01,
+         2.5615e-01,  2.9474e-02,  1.0649e-01,  8.5979e-01,  2.9673e-02,
+         9.4821e-03,  7.6586e-03,  9.3396e-02,  3.2432e-01,  6.7568e-01,
+         1.0877e-08, -8.6002e-09, -9.8649e-09,  9.1188e-01,  5.9678e-04,
+         2.8513e-03,  3.6138e-03,  3.5475e-03,  4.2438e-03,  4.6416e-04,
+         3.3154e-03,  1.9561e-03,  9.6147e-04,  2.3540e-03,  2.2545e-03,
+         3.0502e-03,  1.3925e-03,  3.9785e-04,  6.2330e-03,  2.2545e-03,
+         1.8566e-03,  2.6523e-03,  2.1219e-03,  2.0224e-02,  1.1272e-03,
+         7.9570e-04,  8.9517e-04,  2.2213e-03,  5.6362e-04,  8.9517e-04,
+         1.3925e-03,  1.3925e-03,  1.8566e-03,  4.3101e-04,  2.0887e-03,
+         1.0941e-03,  3.6470e-04,  5.6362e-04,  5.3047e-04,  3.3154e-03,
+         5.9678e-04,  9.9463e-04,  6.2993e-04,  3.3154e-05])   # Normalization vector - means
+        
+        self.normalsig = torch.tensor([1.0000, 0.4393, 0.2757, 0.1853, 0.1740, 0.2527, 0.2015, 0.0215, 1.0000,
+        0.3732, 0.4152, 0.1831, 0.4688, 0.1328, 0.1797, 0.2036, 0.1219, 0.1346,
+        0.1111, 0.2259, 0.0706, 0.1626, 0.1108, 0.0973, 0.0386, 1.0000, 0.4989,
+        0.3467, 0.4674, 0.1737, 0.1633, 0.1101, 0.0264, 0.1712, 0.3402, 0.3085,
+        0.3236, 0.3389, 0.3405, 0.2068, 0.2468, 0.3289, 0.1781, 0.2223, 0.0687,
+        0.1446, 0.0173, 0.2108, 0.3552, 0.4924, 0.4365, 0.1691, 0.3085, 0.3472,
+        0.1697, 0.0969, 0.0872, 0.2910, 0.4681, 0.4681, 1.0000, 1.0000, 1.0000,
+        0.2835, 0.0244, 0.0533, 0.0600, 0.0595, 0.0650, 0.0215, 0.0575, 0.0442,
+        0.0310, 0.0485, 0.0474, 0.0551, 0.0373, 0.0199, 0.0787, 0.0474, 0.0430,
+        0.0514, 0.0460, 0.1408, 0.0336, 0.0282, 0.0299, 0.0471, 0.0237, 0.0299,
+        0.0373, 0.0373, 0.0430, 0.0208, 0.0457, 0.0331, 0.0191, 0.0237, 0.0230,
+        0.0575, 0.0244, 0.0315, 0.0251, 0.0058])   # Normalization vector - stand devs
+
+    def normalize(self, x):
+        '''Normalize the input data'''
+        x = x - self.normalmu
+        x = torch.div(x,self.normalsig)
+        return x
+
+    def smoothing(self, smooth):
+        '''Set smoothing variable. Boolean True/False'''
+        self.smooth = smooth
+    
+    def add_noise(self, x):
+        '''Add noise to input if self.smooth is true. Adapted from LWRS code'''
+        if self.smooth == True:
+            epsilon = self.sigma * torch.randn_like(x)
+            epsilon = epsilon*self.smoothvars
+            return x + epsilon
+        return x
+    
+    def forward (self, x):
+        '''Forwarding function. Normalizes input, and will always go to noising
+        function which handles whether or not to add noise.'''
+        x = self.normalize(x)
+        x = self.add_noise(x)
+        
+        return x
